7f2042b10eb400a1c9d2b5768e9cf4acfa8236df
ykishi0531 <ykishi0531@gmail.com>
2017-01-07 01:57:07 +0900

    Constへコメント追記

--- app/src/main/java/com/example/testapp/common/Const.java
+++ app/src/main/java/com/example/testapp/common/Const.java
@@ -2,13 +2,21 @@ package com.example.testapp.common;
 
 public class Const {
 
+    /** 年月フォーマット */
     public static final String ymdJP = "yyyy年MM月dd日";
+    /** 時分秒フォーマット */
     public static final String hhmmss = "HH:mm:ss";
 
+    /** 打刻区分: 未打刻 */
     public static final String STAMPING_DIVISION_NONE = "00";
+    /** 打刻区分: 出勤 */
     public static final String STAMPING_DIVISION_ATTENDANCE = "01";
+    /** 打刻区分: 退勤 * */
     public static final String STAMPING_DIVISION_LEAVING = "02";
 
+    /** 退勤表示名 */
     public static final String LEAVING_TEXT = "退勤";
-        public static final String LEAVED_TEXT = "おつかれさま";
+
+
+    public static final String LEAVED_TEXT = "おつかれさま";
 }
