acebd9e778e8c1cf1d88cd1bb3bcf775414ffa0a
ykishi0531 <ykishi0531@gmail.com>
2017-01-07 01:40:54 +0900

    viewがそれっぽくなった

--- app/src/main/java/com/example/testapp/Stamping.java
+++ app/src/main/java/com/example/testapp/Stamping.java
@@ -1,12 +1,34 @@
 package com.example.testapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.testapp.common.Const;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Stamping extends AppCompatActivity {
+
+    private Handler mHandler;
+    private Timer mTimer;
+    private static SimpleDateFormat ymd = new SimpleDateFormat(Const.ymdJP);
+    private static SimpleDateFormat hms = new SimpleDateFormat(Const.hhmmss);
+
+    private static String stampingDivision = Const.STAMPING_DIVISION_NONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +36,52 @@ public class Stamping extends AppCompatActivity {
         setContentView(R.layout.activity_stamping);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mHandler = new Handler(getMainLooper());
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        String nowYmd = ymd.format(System.currentTimeMillis());
+                        String nowDate = hms.format(System.currentTimeMillis());
+
+                        ((TextView) findViewById(R.id.ymd)).setText(nowYmd);
+                        ((TextView) findViewById(R.id.hms)).setText(nowDate);
+
+                    }
+                });
+            }
+        },0,1000);
+
+        findViewById(R.id.stamp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nowDate = hms.format(System.currentTimeMillis());
+                if( Const.STAMPING_DIVISION_NONE.equals(stampingDivision) ) {
+                    ((TextView) findViewById(R.id.attendance)).setText(nowDate);
+                    stampingDivision = Const.STAMPING_DIVISION_ATTENDANCE;
+                    ((TextView) findViewById(R.id.stamp)).setText(Const.LEAVING_TEXT);
+                    findViewById(R.id.stamp).setBackgroundResource(R.drawable.shape_rounded_corners_5dp_attendanced);
+                }
+                else if( Const.STAMPING_DIVISION_ATTENDANCE.equals(stampingDivision)){
+                    ((TextView) findViewById(R.id.leaving)).setText(nowDate);
+                    stampingDivision = Const.STAMPING_DIVISION_LEAVING;
+                    ((TextView) findViewById(R.id.stamp)).setText(Const.LEAVED_TEXT);
+                    findViewById(R.id.stamp).setBackgroundResource(R.drawable.shape_rounded_corners_5dp_reaved);
+                }
+                // 以下デバッグ用
+                else {
+                    ((TextView) findViewById(R.id.attendance)).setText("--");
+                    ((TextView) findViewById(R.id.leaving)).setText("--");
+                    stampingDivision = Const.STAMPING_DIVISION_NONE;
+                    ((TextView) findViewById(R.id.stamp)).setText("出勤");
+                    findViewById(R.id.stamp).setBackgroundResource(R.drawable.shape_rounded_corners_5dp);
+                }
+            }
+        });
     }
 
     @Override
@@ -36,5 +104,14 @@ public class Stamping extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if( mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+        }
     }
 }
---
+++ app/src/main/java/com/example/testapp/common/Const.java
@@ -0,0 +1,14 @@
+package com.example.testapp.common;
+
+public class Const {
+
+    public static final String ymdJP = "yyyy年MM月dd日";
+    public static final String hhmmss = "HH:mm:ss";
+
+    public static final String STAMPING_DIVISION_NONE = "00";
+    public static final String STAMPING_DIVISION_ATTENDANCE = "01";
+    public static final String STAMPING_DIVISION_LEAVING = "02";
+
+    public static final String LEAVING_TEXT = "退勤";
+        public static final String LEAVED_TEXT = "おつかれさま";
+}
