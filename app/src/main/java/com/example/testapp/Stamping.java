6216c010993df2e1e8f6cd2eb1b630868aee658d
ykishi0531 <ykishi0531@gmail.com>
2017-01-07 01:46:01 +0900

    不要なimport文を削除

--- app/src/main/java/com/example/testapp/Stamping.java
+++ app/src/main/java/com/example/testapp/Stamping.java
@@ -1,23 +1,17 @@
 package com.example.testapp;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.testapp.common.Const;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
